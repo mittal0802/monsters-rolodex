1bca6c6372f020865060f173779df3e176ea6f2a
mittal0802 <mittalkeshav41@gmail.com>
2023-07-15 12:53:00 +0530

    Updating whole project from Class components to Functional Components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CardList from './components/card-list/card-list.component';
 const App = () => {
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
-
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users').
       then(response => response.json()).
@@ -15,9 +15,13 @@ const App = () => {
       })
   }, [])
 
-  const filteredMonsters = monsters.filter((monster) =>{
+  useEffect(()=>{
+     const newfilteredMonsters = monsters.filter((monster) =>{
         return monster.name.toLocaleLowerCase().includes(searchField);
     })
+    setFilteredMonsters(newfilteredMonsters);
+  }, [monsters, searchField])
+  
   
   const onSearchChange = (event) => {
     const searchString = event.target.value.toLocaleLowerCase();
